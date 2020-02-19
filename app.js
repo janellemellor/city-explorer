69dcd3db66df8b5887b76452df6de96db18faddb
janelle mellor <janellemellor@gmail.com>
2020-02-19 11:15:26 -0800

    add weather endpoint

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
 const express = require('express');
-const data = require('./geo.json');
+const geoData = require('./geo.js');
+const weatherData = require('./darksky.js')
 const app = express();
-const request = require('superagent');
+// const request = require('superagent');
 const port = process.env.PORT || 3000;
 
 // console.log(data)
 
-
+//set state
+let lat;
+let lng;
 
 app.get('/location', (request, respond) => {
-  const cityData = data.results[0];
-  
-  respond.json({
-    formattedQuery: cityData.formatted_address,
-    latitude: cityData.geometry.location.lat,
-    longitutde: cityData.geometry.location.long,
+    const location = request.query.search;
 
-})
-}
+    //will use this when we hit the API
+    console.log('using location...', location);
+
+    const cityData = geoData.results[0];
+
+    //update state so it is accessible in oither routes
+    lat = cityData.geometry.location.lat;
+    lng = cityData.geometry.location.long;
+  
+    respond.json({
+        formattedQuery: cityData.formatted_address,
+        latitude: cityData.geometry.location.lat,
+        longitutde: cityData.geometry.location.long,
+    });
+});
+
+//will use lat and lng below when we hit the api
+const getWeatherData = (lat, lng) => {
+    return weatherData.daily.data.map(forecast => {
+        return {
+            forecast: forecast.summary,
+            //new Data is standard. Time is in seconds in API so must convert
+            time: new Date(forecast.time * 1000),
+        };
+    });
+};
+
+app.get('./weather', (req, res) => {
+    //use the lat and lng from earlier to get weather data for the selected area
+    const portlandWeather = getWeatherData(lat,lng); 
+
+    //res.json that weather data in the appropriate form
+    res.json(portlandWeather);
+});
+
+
 
 
 app.listen(3000, () => {console.log('running ...')})
