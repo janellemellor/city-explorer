8d5cf52c90eb4f92be1b7aa9e0e34e918d418f3c
janelle mellor <janellemellor@gmail.com>
2020-02-19 14:39:29 -0800

    update scripts

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const geoData = require('./geo.js');
+// const geoData = require('./geo.js');
 const weatherData = require('./darksky.js');
 const app = express();
-// const request = require('superagent');
-// const port = process.env.PORT || 3000;
-// const dotenv = require
-
-//add cors?
+const request = require('superagent');
+const port = process.env.PORT || 3000;
+require('dotenv').config();
 
 //middleware - shoving things onto the request
 // app.use((req, res, next) => {
@@ -25,52 +23,43 @@ app.get('/location', async(req, res, next) => {
     try {
     
     //get location from query params
-    //     const location = req.query.search;
-
+        const location = req.query.search;
+        
     //     //hide Key!
-    //     const URL = 'api link goes here'
-    //     // GET https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json
+        const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
 
 
+        const cityData = await request.get(URL);
+        const firstResult = cityData.body[0];
 
-    // //will use the below data when we actually hit the API
-    //     console.log('using location...', location);
+        console.log(cityData.body);
 
+        lat = firstResult.lat;
+        lng = firstResult.lon;
 
-    //     const cityData = await request.get(URL);
-
-
-
-    //     console.log(cityData.body);
-
-    //     lat = firstResult.lat;
-    //     lng = firstResult.lon;
-
-
-        // res.json({
-        //     formatted_query: firstResult, 
-        //     display_name, 
-        //     latitude: lat, 
-        //     longtitude: lng,
-        // })
-       
-
-    //using the hardcoded geo data
-        const cityData = geoData.results[0];
-
-    //update state so it is accessible in oither routes
-        lat = cityData.geometry.location.lat;
-        lng = cityData.geometry.location.lng;
-  
         res.json({
-            formattedQuery: cityData.formatted_address,
-            latitude: cityData.geometry.location.lat,
-            longitutde: cityData.geometry.location.lng,
+            formatted_query: firstResult.display_name,
+            latitude: lat, 
+            longtitude: lng,
         });
+       
     } catch (err) {
         next(err);
     }
 });
+    // //Below code is using the hardcoded geo data
+    //     const cityData = geoData.results[0];
+
+    // //update state so it is accessible in oither routes
+    //     lat = cityData.geometry.location.lat;
+    //     lng = cityData.geometry.location.lng;
+  
+        // res.json({
+        //     formattedQuery: cityData.formatted_address,
+        //     latitude: cityData.geometry.location.lat,
+        //     longitutde: cityData.geometry.location.lng,
+        
+   
 
 //will use lat and lng below when we hit the api
 const getWeatherData = (lat, lng) => {
@@ -83,7 +72,7 @@ const getWeatherData = (lat, lng) => {
     });
 };
 
-app.get('./weather', (req, res) => {
+app.get('/weather', (req, res) => {
     //use the lat and lng from earlier to get weather data for the selected area
     const portlandWeather = getWeatherData(lat, lng); 
 
