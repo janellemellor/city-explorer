651bbc95aca28f231e575b7d3280d47f2347a044
janelle mellor <janellemellor@gmail.com>
2020-02-22 21:51:55 -0800

    delete misc comments

--- app.js
+++ app.js
@@ -3,26 +3,14 @@ const app = express();
 const request = require('superagent');
 require('dotenv').config();
 
-//middleware - shoving things onto the request
-// app.use((req, res, next) => {
-//     console.log(req);
-//     next;
-// });
-
-// console.log(data)
-
-//set state of lat and long so it can be used globally
 let lat;
 let lng;
 
 
 app.get('/location', async(req, res, next) => {
     try {
-    
-    //get location from query params
         const location = req.query.search;
-        
-    //     //hide Key!
+    
         const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${location}&format=json`;
 
 
@@ -42,19 +30,8 @@ app.get('/location', async(req, res, next) => {
         next(err);
     }
 });
-    // //Below code is using the hardcoded geo data
-    //     const cityData = geoData.results[0];
 
-    // //update state so it is accessible in oither routes
-    //     lat = cityData.geometry.location.lat;
-    //     lng = cityData.geometry.location.lng;
-  
-        // res.json({
-        //     formattedQuery: cityData.formatted_address,
-        //     latitude: cityData.geometry.location.lat,
-        //     longitutde: cityData.geometry.location.lng,
-        
-//will use lat and lng below when we hit the api
+
 const getWeatherData = async(lat, lng) => {
    
     const weather = await request.get(`https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${lng}`);
@@ -62,7 +39,6 @@ const getWeatherData = async(lat, lng) => {
     return weather.body.daily.data.map(forecast => {
         return {
             forecast: forecast.summary,
-            //new Data is standard. Time is in seconds in API so must convert
             time: new Date(forecast.time * 1000),
         };
     });
@@ -70,10 +46,8 @@ const getWeatherData = async(lat, lng) => {
 
 app.get('/weather', async(req, res, next) => {
     try {
-    //use the lat and lng from earlier to get weather data for the selected area
         const portlandWeather = await getWeatherData(lat, lng); 
 
-    //res.json that weather data in the appropriate form
         res.json(portlandWeather);
     } catch (err) {
         next(err);
@@ -95,7 +69,6 @@ const getYelpData = async(lat, lng) => {
         };
     });
 };
-
 
 app.get('/restaurants', async(req, res, next) => {
     try {
@@ -167,7 +140,6 @@ app.get('/events', async(req, res, next) => {
     }
 });
 
-//404 route must be at the end of the routes or the route will default here. 
 app.get('*', (request, respond) => {
     respond.send('404');
 });
